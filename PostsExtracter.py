8b814ec73dcb181b16182cc7a366d892f698a5a5
Claudio Agosti <claudio@tracking.exposed>
2017-10-22 00:32:55 +0200

    fixes on the post crawler

--- PostsExtracter.py
+++ PostsExtracter.py
@@ -9,41 +9,52 @@ import facebook
 import json
 from urllib.request import urlopen 
 
-Graph = facebook.GraphAPI(access_token="EAACEdEose0cBAH6T9vLzyFR3jW7OqxWbucvr87S2QrU7XHwdqZAXdNhl9tbqMFN3DNv5PLL8GsfBdx80kG8k5hcaTmbFuLHAEX0yltPIsCIUf22xqACLD6OCg6whe0eZCOVszwQopXmW9QDiFqbyklQDapCstC3dcZBd5J5hMtBH4aGnqqcUFLUaNmJop8ZD", version=2.10)
+Graph = facebook.GraphAPI(access_token="EAACEdEose0cBAEAzbavyYnd1HTijPrtzSO12v2CqAq3YhRexZBdNStFJKx3VklkZByD9UWWQdeDHlap1Gxo61nDOYi6UdVSCOMVlr5Ac6sUswGmwJlU7iEM6dwLFL1ePq2o3XIosuQit8j4OVULv04QZACYJgvL9YojKVe03Dcd1yc98YlZCHDZCkt5FgmUEZD", version=2.10)
 
+listaId = [ "107213789359298", "109876399068362", "115689108495633", "118826701482992", "118997844848271", "121157534566507", "135288816587085", "138825709486612", "142047174531", "142725972411814", "143064249067032", "144178662273696", "1535010270052051", "1541638399393436", "155703934164", "156057357762712", "156401394376218", "1584691088491568", "159224630305", "167965119890296", "169958736347670", "174586312591604", "193795663980288", "202475239772616", "207539002644269", "213440425391495", "223106311128525", "237157814277", "238778949515431", "263647057133329", "28963119862", "31783122271", "324055054319413", "329205457033", "34839376970", "369419599554", "374082229927", "45947678873", "484779765384", "51631931862", "71339054219", "87545580838", "938271029528393" ]
 
+# listaId = [ "142725972411814" ]
 
-
-def nextfetch(inurl):
-    inurl = "https://graph.facebook.com/v2.5/28963119862/posts?limit=100&access_token=EAACEdEose0cBAMcAFwrTUyTuduDPPikmGtwnA790YhyIpO6bV6qyerOO75H6Fj9uFvEyDsZAaPRoZAwRCAYJoXN0O6bWRIvcqZA8mUCQmHi58ue8P3poi6bOFx0ESbuBiaSm9HkKnqutmepkiWZASfx7biZBDF3QtJo0MEjIpyDOQPjZBNmBiso5G1ZCFNzbyUZD&until=1508017321&__paging_token=enc_AdDF6eo4cD1kc6geh8SrrTAgZBhF0WtnXBFx6A5fUaRE6wxfTcWBfoF9JmZBCZAGj9RsWk6KU3HReaepljkoRiUTo2W"
-    return json.loads(urlopen(inurl).read())
-    
 def saveFile(filesuffix, content):
     fp = open("fbrex" + filesuffix + ".json", 'w+')
     json.dump(content, fp)
     fp.close()
-    
+
+tutto = list()
+
+def processing(sourceId, sourceName, posts, nextURL):
+    print("Processing %d post" % len(posts))
+
+    keepgoing = True;
+
+    for sp in posts:
+        if sp['created_time'][5:7] == '10' and int(sp['created_time'][8:10]) >= 7:
+            print("saving post created on %s by %s" % (sp['created_time'], sourceName) )
+            sp.update({"sourceName": sourceName})
+            sp.update({"sourceId": sourceId})
+            tutto.append(sp)
+        else:
+            print("stop recursion, found post made in %s" % sp['created_time'])
+            keepgoing = False
+
+    if keepgoing:
+        print("Recursion via %s" % nextURL);
+        rawdata = urlopen(nextURL).read().decode('utf8')
+        newposts = json.loads(rawdata)
+        nextURL = newposts['paging']['next']
+        processing(sourceId, sourceName, newposts['data'], nextURL)
 
 
-posts = Graph.get_object(id="28963119862", fields='posts.limit(100))
+print ("Beginning!!")
+for pageid in listaId:
+    print ("Processing %s" % pageid)
+    posts = Graph.get_object(id=pageid, fields="posts.limit(100),name")
+    sourceName = posts['name']
+    nextURL = posts['posts']['paging']['next']
+    processing(pageid, sourceName, posts['posts']['data'], nextURL)
 
+print ("Saving!!")
+with open("/tmp/tuttipost.json", "w") as fp:
+    json.dump(tutto, fp)
 
-
-saveFile("inizio", posts)
-posts.keys()
-next = posts['paging']['next']
-print(next)
-suffisso = 2
-while next:
-    altro = nextfetch(next)
-    saveFile(suffisso, altro)
-    next = altro['paging']['next']
-    suffisso = suffisso - 1
-    if not next:
-        break        
-    
-
-
-
-
-
+print ("End!!")
--- ToRank.py
+++ ToRank.py
@@ -9,7 +9,7 @@ import facebook
 import json
 
 
-graph = facebook.GraphAPI(access_token='', version=2.10)
+graph = facebook.GraphAPI(access_token='EAACEdEose0cBAEQakpiClVsX2s9H5KqTZBBDZBmARCRXMiv6JJ1Oz0ssdmZAMzEqjX4ZAGbBDVhTHjNaFdlbBJKo2C22XRRN4OZAXR5u1eZBoq9KZBJxvHOJZBjDzuGB0u3IadNkbFPxUHBbY1QSaKrS7xZCtyslZBtYZAoQmZCo4bu5IhqtzeXMhAdSmWw48obBnNk3MCZAFNmhBZAwZDZD', version=2.10)
 #new token, "https://developers.facebook.com/tools/explorer/145634995501895/?method=GET&path=28963119862%3Ffields%3Dposts.limit(100)&version=v2.10"
 
 def saveFile(filesuffix, content):
